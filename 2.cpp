c62b06381351b9f3b1088cb31782446e58249bd7
Haryce <148765923+Haryce@users.noreply.github.com>
2024-09-12 12:45:08 +0900

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+
+    setlocale(LC_ALL, "Russian"); // добавляем русский язык
+    cout << "Напишите емкость аккамулятора: \n";
+    int a, b;// создаем 2 переменые
+    cin >> a;
+    b = a * 3;
+    cout << "Емкость подходящего вам блока питания: \n" << b; // выводим результат
+    
+
+
+    return 0;
+}
